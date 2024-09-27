13e94c9fe7ba4427cda2e00e2b5fff458b222cc4
Gerardo Britez <gerardobritez.gb@gmail.com>
2024-09-27 12:23:57 -0300

    route versions

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\TypeSpaceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-
+use App\Http\Controllers\Api\SpaceController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,16 +16,8 @@ use App\Http\Controllers\Api\AuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-//login con jwt 
-Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware(['jwt.auth'])->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::get('user', function () {
-        return auth()->user();
-    });
-});
+Route::prefix('v1')->group(base_path('routes/api_versions/api_v1.php'));
---
+++ back/routes/api_versions/api_v1.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\SpaceController;
+use App\Http\Controllers\Api\TypeSpaceController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['jwt.auth'])->group(function () {
+    
+    Route::post('logout', [AuthController::class, 'logout']);
+    
+    // Routes only admin
+    Route::middleware(['role:admin'])->group(function() {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::apiResource('spaces', SpaceController::class)->except(['create', 'edit']);
+        Route::apiResource('types', TypeSpaceController::class)->except(['create', 'edit', 'destroy']);
+    });
+
+    // Routes for admin and assistant
+    Route::middleware(['role:admin,assistant'])->group(function() {
+        Route::get('user', function () {
+            return auth()->user();
+        });
+    });
+});
